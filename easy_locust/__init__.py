90e22e7a762f960587c20b929f40cdf39b882373
GuoTengda1993 <guotengdaxx@gmail.com>
2020-05-20 00:09:42 +0800

    index.html placeholder

--- easy_locust/__init__.py
+++ easy_locust/__init__.py
@@ -1,7 +1,7 @@
 # from locust.core import HttpLocust, Locust, TaskSet, TaskSequence, task, seq_task
 # from locust.exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
 from .util.locustFileFactory import make_locustfile
-__version__ = "0.2.1"
+__version__ = "0.2.1.1"
 
 
 def factory(data):
